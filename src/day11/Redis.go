dbbeb406e664a3167b165217791b168b55681009
yige666s <yige29805@gmail.com>
2024-06-20 00:22:16 +0800

    pipeline,Transcation

--- src/day11/Redis.go
+++ src/day11/Redis.go
@@ -131,4 +131,97 @@ func ZsetTest() {
 	}
 }
 
-// TODO 扫描或遍历所有key
+// 扫描或遍历所有key
+// 在Redis中可以使用KEYS prefix* 命令按前缀查询所有符合条件的 key，
+// go-redis库中提供了Keys方法实现类似查询key的功能。
+// 针对这种需要遍历大量key的场景，go-redis中提供了一个简化方法——Iterator
+// delKeysByMatch 按match格式扫描所有key并删除
+func delKeysByMatch(match string, timeout time.Duration) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	iter := rdb.Scan(ctx, 0, match, 0).Iterator()
+	// iter := rdb.SScan(ctx, "set-key", 0, "perfix:*", 0).Iterator()		// set
+	// iter := rdb.HScan(ctx, "hash-key", 0, "perfix:*", 0).Iterator()        // hash
+	// iter := rdb.ZScan(ctx, "sorted-hash-key", 0, "prefix:*", 0).Iterator() //zset
+	for iter.Next(ctx) {
+		err := rdb.Del(ctx, iter.Val()).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+	if err := iter.Err(); err != nil {
+		panic(err)
+	}
+}
+
+// Redis Pipeline 允许通过使用单个 client-server-client 往返执行多个命令来提高性能
+// 节省了执行命令的网络往返时间（RTT）。
+// 在那些我们需要一次性执行多个命令的场景下，就可以考虑使用 pipeline 来优化。
+func PipelinedTest() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	cancel()
+
+	// Pipelined 方法会在函数退出时调用 Exec。
+	var incr *redis.IntCmd
+	var boolres *redis.BoolCmd
+	_, err := rdb.Pipelined(ctx, func(p redis.Pipeliner) error {
+		incr = p.Incr(ctx, "pipelined_counter")
+		boolres = p.Expire(ctx, "pipelined_counter", time.Hour)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	// 在pipeline执行后获取到结果
+	fmt.Println(incr.Val(), boolres.Val())
+
+	// 可以遍历 pipeline 命令的返回值依次获取每个命令的结果
+	cmds, _ := rdb.Pipelined(ctx, func(p redis.Pipeliner) error {
+		for i := 0; i < 100; i++ {
+			p.Get(ctx, fmt.Sprintf("key%d", i))
+		}
+		return nil
+	})
+
+	for _, cmdres := range cmds {
+		// cmdres被类型断言为*redis.StringCmd类型。类型断言是Go语言中的一种操作，用于将接口类型的变量转换为具体的类型。
+		// 语法是：x.(T)，其中x是接口类型的变量，T是你要断言的具体类型。如果断言成功，结果就是具体类型的值；如果失败，会引发panic。
+		fmt.Println(cmdres.(*redis.StringCmd).Val())
+	}
+}
+
+// Multi/exec能够确保在multi/exec两个语句之间的命令之间没有其他客户端正在执行命令。
+// 在这种场景我们需要使用 TxPipeline 或 TxPipelined 方法将 pipeline 命令使用 MULTI 和EXEC包裹起来。
+// 通常搭配 WATCH命令来执行事务操作。从使用WATCH命令监视某个 key 开始，
+// 直到执行EXEC命令的这段时间里，如果有其他用户抢先对被监视的 key 进行了替换、更新、删除等操作，
+// 那么当用户尝试执行EXEC的时候，事务将失败并返回一个错误，用户可以根据这个错误选择重试事务或者放弃事务。
+func watchDemo(ctx context.Context, key string, rdb *redis.Client) error {
+	return rdb.Watch(ctx, func(tx *redis.Tx) error {
+		n, err := tx.Get(ctx, key).Int() // 获取key的值
+		if err != nil && err != redis.Nil {
+			return err
+		}
+
+		// 假设操作耗时5秒,5秒内我们通过其他的客户端修改key，当前事务就会失败
+		time.Sleep(5 * time.Second)
+
+		_, err = tx.Pipelined(ctx, func(p redis.Pipeliner) error {
+			p.Set(ctx, key, n+1, time.Hour) // 修改key的值
+			return nil
+		})
+		return err
+	}, key)
+}
+
+// go-redis 官方文档中使用 GET 、SET和WATCH命令实现一个 INCR 命令的完整示例
+// TODO go-redis INCR
