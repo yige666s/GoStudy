ba783200daf86df752bf244534c287cb9dba289e
yige666s <yige29805@gmail.com>
2024-06-20 23:14:29 +0800

    air hotreload

---
+++ src/day13/Air.go
@@ -0,0 +1,8 @@
+package main
+
+// Air能够实时监听项目的代码文件，在代码发生变更之后自动重新编译并执行，大大提高gin框架项目的开发效率。
+// go install github.com/air-verse/air@latest
+// 在你的项目根目录下创建一个 .air.toml 配置文件（可以通过 air init 命令生成默认配置）：
+// air init
+// 然后启动 Air：
+// air
--- src/day13/main.go
+++ src/day13/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/hello", func(ctx *gin.Context) {
+		fmt.Println("hello,world")
+	})
+	r.Run(":8080")
 }
