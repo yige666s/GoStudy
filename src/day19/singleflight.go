885e17a68dadd6053fbcfb6c3e3b60acda20af3a
dingliguo1 <dingliguo1@tal.com>
2024-06-27 18:32:19 +0800

    singleflight

--- src/day19/singleflight.go
+++ src/day19/singleflight.go
@@ -1,3 +1,66 @@
 package main
 
-// TODO singleflight处理缓存击穿
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/singleflight"
+)
+
+// singleflight处理缓存击穿
+// 它提供了重复函数调用抑制机制，使用它可以避免同时进行相同的函数调用。
+// 第一个调用未完成时后续的重复调用会等待，当第一个调用完成时则会与它们分享结果，
+// 这样以来虽然只执行了一次函数调用但是所有调用都拿到了最终的调用结果。
+
+func getData(id int64) string {
+	fmt.Println("query...")
+	time.Sleep(10 * time.Second) // 模拟一个比较耗时的操作
+	return "hello world!"
+}
+func DoChanGetData(ctx context.Context, g *singleflight.Group, id int64) (string, error) {
+	// DoChan第二个参数是对 key函数"getData"的逻辑封装
+	ch := g.DoChan("getData", func() (interface{}, error) { // 在没有其他正在进行的相同 key 的调用时应执行的实际操作
+		go func() {
+			time.Sleep(100 * time.Millisecond) // 100ms后忘记key,后续重复调用可进行
+			g.Forget("getData")
+		}()
+		ret := getData(id)
+		return ret, nil
+	})
+	// 为了避免第一次调用阻塞所有调用的情况，我们可以结合使用select和DoChan为函数调用设置超时时间。
+	select {
+	case <-ctx.Done(): // 如果 context 被取消（例如由于超时或手动取消），返回一个错误，表示操作被取消。
+		return "", ctx.Err()
+	case ret := <-ch: // 如果 DoChan 调用完成，返回结果和错误（如果有的话）。
+		return ret.Val.(string), ret.Err
+	}
+}
+
+func SingleflightTest() {
+	g := new(singleflight.Group)
+	// 第一次调用
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		v1, err := DoChanGetData(ctx, g, 1)
+		fmt.Printf("v1:%v err:%v\n", v1, err)
+	}()
+
+	time.Sleep(2 * time.Second)
+
+	// 第二次调用无法进行，等待第一次调用结束赋值
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	v2, err := DoChanGetData(ctx, g, 1)
+	fmt.Printf("v2:%v err:%v\n", v2, err)
+}
+
+// singleflight将并发调用合并成一个调用的特点决定了它非常适合用来防止缓存击穿。
+func getDataSingleFlight(key string) (interface{}, error) {
+	g := new(singleflight.Group)
+	// 查缓存
+	v, err := g.Do(key, func() (interface{}, error) {
+
+	})
+}
